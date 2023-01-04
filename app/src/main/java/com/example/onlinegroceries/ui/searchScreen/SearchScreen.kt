2fe272b932f264b82f412dd20c8e73c7a0675725
khaled eid <khaled.eid1k1k@gmail.com>
2023-01-04 17:14:52 +0200

    MVVM - Repositry pattern - coroutine - Glid - ViewModelFactory

--- app/src/main/java/com/example/onlinegroceries/ui/searchScreen/SearchScreen.kt
+++ app/src/main/java/com/example/onlinegroceries/ui/searchScreen/SearchScreen.kt
@@ -2,7 +2,10 @@ package com.example.onlinegroceries.ui.searchScreen
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,28 +17,31 @@ import com.example.onlinegroceries.databinding.ActivitySearchScreenBinding
 import com.example.onlinegroceries.ui.main.MyViewModelFactory
 import com.example.onlinegroceries.ui.main.ProductsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class SearchScreen : AppCompatActivity() {
+
     private lateinit var recyclerView: RecyclerView
-    private lateinit var searchAdapter: SearchAdapter
+     private lateinit var searchAdapter: SearchAdapter
     private lateinit  var binding : ActivitySearchScreenBinding
-    private lateinit var viewModel: ProductsViewModel
-    private lateinit var mainRepository  : MainRepository
+     private val viewModel by viewModels<ProductsViewModel> {
+         MyViewModelFactory(mainRepository)
+     }
+     private val mainRepository  by lazy {
+          MainRepository()
+     }
+//private  val viewModel : ProductsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySearchScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // Hidden toolbar
         supportActionBar?.hide()
 
         // initialize  recyclerView
         recyclerView=binding.searchItems
-
-        val retrofitService = RetrofitBuilder
-
-          //initialize Repository
-         mainRepository = MainRepository(retrofitService)
 
         observeViewModel()
 
@@ -43,9 +49,9 @@ class SearchScreen : AppCompatActivity() {
 
 
     private fun observeViewModel(){
-        viewModel = ViewModelProvider(this,
-            MyViewModelFactory(mainRepository))[ProductsViewModel::class.java]
+
         viewModel.getAllProducts()
+
         viewModel.productsList.observe(this) {
             installViews(it)
         }
@@ -57,15 +63,15 @@ class SearchScreen : AppCompatActivity() {
     private fun installViews(productList: ProductModel){
         val layoutManager: RecyclerView.LayoutManager =
             GridLayoutManager(this,2)
-
-        // to make space between columns
-        recyclerView.addItemDecoration(
-             GridSpacingItemDecoration(
-                2,
-                100,
-                10
-            )
-        )
+//
+//        // to make space between columns
+//        recyclerView.addItemDecoration(
+//             GridSpacingItemDecoration(
+//                2,
+//                100,
+//                10
+//            )
+//        )
         recyclerView.layoutManager = layoutManager
         searchAdapter = SearchAdapter(productList,applicationContext)
         recyclerView.adapter = searchAdapter
