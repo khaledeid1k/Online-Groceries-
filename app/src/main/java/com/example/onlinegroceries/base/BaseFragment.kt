7a1a62e4352c5dbaea113108ff12b93f9e003db2
khaled eid <khaled.eid1k1k@gmail.com>
2023-05-05 18:34:55 +0300

    final DataBinding to BaseFragment

--- app/src/main/java/com/example/onlinegroceries/base/BaseFragment.kt
+++ app/src/main/java/com/example/onlinegroceries/base/BaseFragment.kt
@@ -6,15 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 import com.example.onlinegroceries.ui.home.ProductsViewModel
 
-abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
     abstract val LOG_TAG: String
-    private var _binding: ViewBinding? = null
+    private var _binding: ViewDataBinding? = null
     abstract fun getLayoutId(): Int
 
 
