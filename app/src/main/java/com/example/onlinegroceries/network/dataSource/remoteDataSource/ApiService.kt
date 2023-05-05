0ec5c4c45f76240a227026b068dffb3d20680d3c
khaled eid <khaled.eid1k1k@gmail.com>
2023-05-05 18:11:16 +0300

    change interface of api

--- app/src/main/java/com/example/onlinegroceries/network/dataSource/remoteDataSource/ApiService.kt
+++ app/src/main/java/com/example/onlinegroceries/network/dataSource/remoteDataSource/ApiService.kt
@@ -8,14 +8,12 @@ import retrofit2.http.Path
 
 
 interface ApiService {
+    // this interface responsible for end point according for function you add above of
+    // method
     @GET("Products")
     suspend fun getProducts(): Response<ProductResponse>
-
-
     @GET("Products/{id}")
-    suspend fun getSearch(
-        @Path("id") id: Int
-    ): Response<SearchModels>
+    suspend fun getSearch(@Path("id") id: Int): Response<SearchModels>
 }
 
 
